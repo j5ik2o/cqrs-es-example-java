5c2fc02f8cbd40e679a0209aed33c54ef119b78d
Junichi Kato <j5ik2o@gmail.com>
2024-03-06 09:22:56 +0900

    fix: add @QueryMapping

--- query/interface-adaptor/src/main/java/com/github/j5ik2o/cqrs/es/java/query/interface_adaptor/graphql/QueryResolverImpl.java
+++ query/interface-adaptor/src/main/java/com/github/j5ik2o/cqrs/es/java/query/interface_adaptor/graphql/QueryResolverImpl.java
@@ -3,7 +3,10 @@ package com.github.j5ik2o.cqrs.es.java.query.interface_adaptor.graphql;
 import com.github.j5ik2o.cqrs.es.java.query.interface_adaptor.dao.GroupChatMapper;
 import com.github.j5ik2o.cqrs.es.java.query.interface_adaptor.dao.MemberMapper;
 import com.github.j5ik2o.cqrs.es.java.query.interface_adaptor.dao.MessageMapper;
+import org.apache.ibatis.annotations.Arg;
 import org.springframework.context.annotation.Profile;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -24,8 +27,9 @@ public class QueryResolverImpl implements QueryResolver {
     this.messageMapper = messageMapper;
   }
 
+  @QueryMapping
   @Override
-  public Mono<GroupChatOutput> getGroupChat(String groupChatId, String userAccountId)
+  public Mono<GroupChatOutput> getGroupChat(@Argument("groupChatId") String groupChatId, @Argument("userAccountId") String userAccountId)
       throws Exception {
     return Mono.justOrEmpty(
         groupChatMapper
@@ -40,8 +44,9 @@ public class QueryResolverImpl implements QueryResolver {
                         groupChatRecord.updatedAt().toString())));
   }
 
+  @QueryMapping
   @Override
-  public Flux<GroupChatOutput> getGroupChats(String userAccountId) throws Exception {
+  public Flux<GroupChatOutput> getGroupChats(@Argument("userAccountId") String userAccountId) throws Exception {
     return Flux.fromArray(
         groupChatMapper.getGroupChats(userAccountId).stream()
             .map(
@@ -55,8 +60,9 @@ public class QueryResolverImpl implements QueryResolver {
             .toArray(GroupChatOutput[]::new));
   }
 
+  @QueryMapping
   @Override
-  public Mono<MemberOutput> getMember(String groupChatId, String userAccountId) throws Exception {
+  public Mono<MemberOutput> getMember(@Argument("groupChatId") String groupChatId, @Argument("userAccountId") String userAccountId) throws Exception {
     return Mono.justOrEmpty(
         memberMapper
             .getMember(groupChatId, userAccountId)
@@ -71,8 +77,9 @@ public class QueryResolverImpl implements QueryResolver {
                         memberRecord.updatedAt().toString())));
   }
 
+  @QueryMapping
   @Override
-  public Flux<MemberOutput> getMembers(String groupChatId, String userAccountId) throws Exception {
+  public Flux<MemberOutput> getMembers(@Argument("groupChatId") String groupChatId, @Argument("userAccountId") String userAccountId) throws Exception {
     return Flux.just(
         memberMapper.getMembers(groupChatId, userAccountId).stream()
             .map(
@@ -87,8 +94,9 @@ public class QueryResolverImpl implements QueryResolver {
             .toArray(MemberOutput[]::new));
   }
 
+  @QueryMapping
   @Override
-  public Mono<MessageOutput> getMessage(String messageId, String userAccountId) throws Exception {
+  public Mono<MessageOutput> getMessage(@Argument("messageId") String messageId, @Argument("userAccountId") String userAccountId) throws Exception {
     return Mono.justOrEmpty(
         messageMapper
             .getMessage(messageId, userAccountId)
@@ -103,8 +111,9 @@ public class QueryResolverImpl implements QueryResolver {
                         messageRecord.updatedAt().toString())));
   }
 
+  @QueryMapping
   @Override
-  public Flux<MessageOutput> getMessages(String groupChatId, String userAccountId)
+  public Flux<MessageOutput> getMessages(@Argument("groupChatId") String groupChatId, @Argument("userAccountId") String userAccountId)
       throws Exception {
     return Flux.just(
         messageMapper.getMessages(groupChatId, userAccountId).stream()
